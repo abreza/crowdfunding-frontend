fe7fe30e945255bd28d98a5f87036d62ed5e216a
ab.reza <a.re.morteza@gmail.com>
2021-06-12 22:21:49 +0430

    fix project card

--- src/components/molecules/projectCard/ProjectCard.tsx
+++ src/components/molecules/projectCard/ProjectCard.tsx
@@ -1,23 +1,56 @@
 import {
-  Button,
+  Avatar,
   Card,
   CardActionArea,
-  CardActions,
   CardContent,
   CardMedia,
+  Grid,
+  LinearProgress,
   makeStyles,
   Typography,
+  withStyles,
 } from '@material-ui/core';
 import { FC } from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
-});
+  description: {
+    display: '-webkit-box',
+    maxWidth: '100%',
+    WebkitLineClamp: '2' as any,
+    WebkitBoxOrient: 'vertical',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    marginBottom: theme.spacing(3),
+  },
+  linearProgress: {
+    margin: theme.spacing(3, 0, 1),
+  },
+  bold: {
+    fontWeight: 600,
+  },
+}));
+
+const BorderLinearProgress = withStyles((theme) => ({
+  root: {
+    height: 10,
+    borderRadius: 5,
+  },
+  colorPrimary: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+  },
+  bar: {
+    borderRadius: 5,
+    backgroundColor: '#1a90ff',
+  },
+}))(LinearProgress);
 
 const ProjectCard: FC<any> = ({ item }) => {
   const classes = useStyles();
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -32,19 +65,68 @@ const ProjectCard: FC<any> = ({ item }) => {
           <Typography gutterBottom variant="h5" component="h2">
             {item.name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            className={classes.description}>
             {item.description}
           </Typography>
+          <Grid container direction="row" alignItems="center" spacing={2}>
+            <Grid item>
+              <Avatar
+                alt="Robert Jervis"
+                src="https://media.tehrantimes.com/d/t/2020/05/17/4/3454683.jpg"
+              />
+            </Grid>
+            <Grid item>
+              <Grid container direction="column">
+                <Grid item>
+                  <Typography variant="body2" component="p">
+                    Robert Jervis
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography
+                    variant="caption"
+                    color="textSecondary"
+                    component="p">
+                    Professor of Columbia University
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.linearProgress}>
+            <BorderLinearProgress variant="determinate" value={50} />
+            <Grid
+              container
+              justify="space-between"
+              alignItems="center"
+              direction="row">
+              <Grid item>
+                <Typography align="center" className={classes.bold}>
+                  ۳۲٪
+                </Typography>
+                <Typography align="center">سرمایه‌گذاری</Typography>
+              </Grid>
+              <Grid item>
+                <Typography align="center" className={classes.bold}>
+                  ۵۰ میلیون
+                </Typography>
+                <Typography align="center">هدف</Typography>
+              </Grid>
+              <Grid item>
+                <Typography align="center" className={classes.bold}>
+                  ۱۰ روز
+                </Typography>
+                <Typography align="center">تا پایان</Typography>
+              </Grid>
+            </Grid>
+          </div>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 };
--- src/components/organisms/slider/BestProjects.tsx
+++ src/components/organisms/slider/BestProjects.tsx
@@ -8,7 +8,7 @@ import {
 } from '@material-ui/core';
 import { ArrowForward, ArrowBack } from '@material-ui/icons';
 import ChunkedCarousel from 'components/molecules/chunkedCarousel/ChunkedCarousel';
-import { FC, useRef, useState } from 'react';
+import { FC, useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -26,16 +26,17 @@ const BestProjects: FC<{}> = () => {
     {
       image:
         'https://store.makerbot.com/media/catalog/product/cache/75b2125818f23adb3f72d90831c39808/r/e/replicator__1.png',
-      name: '1',
+      name: 'پرینتر ۳ بعدی',
       description:
         'کیفیتی فوق‌العاده و طراحی بسیار دقیق جزئیات! سرعت بالای این محصول امکان استفاده صنعتی از آن را فراهم کرده است.',
     },
     {
       image:
-        'https://store.makerbot.com/media/catalog/product/cache/75b2125818f23adb3f72d90831c39808/r/e/replicator__1.png',
-      name: '2',
-      description:
-        'کیفیتی فوق‌العاده و طراحی بسیار دقیق جزئیات! سرعت بالای این محصول امکان استفاده صنعتی از آن را فراهم کرده است.',
+        'https://yt3.ggpht.com/ytc/AAUvwnhUoqGzslL9NApxwxZ182t9zksg-G69pMlXHhYLAw=s900-c-k-c0x00ffffff-no-rj',
+      name: 'رویداد WSS',
+      description: `چهارمین دوره از سمینار زمستانه مباحث پیشرفته در علوم و مهندسی کامپیوتر (WSS) امسال در تاریخ ۵ و ۶ دی‌ماه مطابق با روال همیشه در دانشگاه صنعتی شریف برگزار می‌گردد.
+        در این سمینار با ارائه‌ی سخنرانی‌های علمی-تخصصی توسط مدعوینی خبره از سراسر دنیا، آخرين دستاوردهای علمی در زمينه‌های مختلف علوم و مهندسی کامپیوتر در قالب یک سمینار دو روزه ارائه می‌گردد. اطلاعات تکمیلی من جمله برنامه‌ی زمانی رویداد و جزئیات سمینارها را می‌توانید از سایت سمینار زمستانه کسب کنید. هم‌چنین اطلاع‌رسانی رویداد از طریق کانال تلگرامی رویداد خواهد بود.
+        از كليه دانشجویان، پژوهشگران و متخصصان دانشگاه‌ها و مؤسسات تحقيقاتی در رشته‌های مرتبط با محورهای سمینار، دعوت می‌شود تا از تاریخ ۱۷ آذرماه تا ۱ دی‌ماه از طریق صفحه‌ی سمینار نسبت به ثبت‌نام اقدام نمایند. با توجه به ظرفیت محدود سمینار، اولیت با افرادی است که زود‌تر ثبت‌نام کنند.`,
     },
     {
       image:
