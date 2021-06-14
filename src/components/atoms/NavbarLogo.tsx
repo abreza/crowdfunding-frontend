2413acde258ee848a9894c1400b1630ad9e48f6b
ab.reza <a.re.morteza@gmail.com>
2021-06-14 17:25:25 +0430

    fix logo size in mobile

--- src/components/atoms/NavbarLogo.tsx
+++ src/components/atoms/NavbarLogo.tsx
@@ -1,12 +1,21 @@
+import UseWindowDimensions from 'components/hoc/UseWindowSize';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const NavbarLogo: FC<{}> = () => {
+  const windowDimensions = UseWindowDimensions();
+
   return (
-    <img
-      src={process.env.PUBLIC_URL + 'logo3.png'}
-      alt="logo"
-      style={{ height: 35 }}
-    />
+    <Link to="/">
+      <img
+        src={
+          process.env.PUBLIC_URL +
+          (windowDimensions.width < 960 ? 'logo2.png' : 'logo3.png')
+        }
+        alt="logo"
+        style={{ height: 35 }}
+      />
+    </Link>
   );
 };
 
