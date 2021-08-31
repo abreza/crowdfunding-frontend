8666b80bdfb2f7f9e9fbb2081c76059ebc939aff
reza <ab.reza@live.com>
2021-08-31 09:37:05 +0430

    change api base url to https

--- src/app/services/baseQuery.ts
+++ src/app/services/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
 import { RootState } from 'app/store';
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://crowdfunding.mamalan.ir/api/v1/',
+  baseUrl: 'https://crowdfunding.mamalan.ir/api/v1/',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
