d63da43c4471b592db88abca154c6fc8840d3041
Morteza Abolghasemi <a.re.morteza@gmail.com>
2021-09-11 11:55:17 +0430

    remove unused packages

--- src/components/atoms/formomponents/FormInputProps.ts
+++
@@ -1,9 +0,0 @@
-import { TextFieldProps } from '@material-ui/core';
-import { Control, UseControllerProps } from 'react-hook-form';
-
-export interface FormInputProps {
-  name: string;
-  control: Control;
-  controllerProps: any;
-  textFieldProps: TextFieldProps;
-}
