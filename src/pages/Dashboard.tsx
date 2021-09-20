953be58f6bf79e2d4831511261ec8212ffea573a
ab.reza <a.re.morteza@gmail.com>
2021-09-20 08:01:03 +0430

    init dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,71 @@
-import { FC } from 'react';
+import {
+  Paper,
+  Container,
+  Typography,
+  Grid,
+  Tabs,
+  Tab,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from '@mui/material';
+import { FC, useState } from 'react';
 import Panel from 'templates/Panel';
 
 type DashboardProps = {};
 
+const tabs = [
+  {
+    label: 'پروفایل',
+  },
+  {
+    label: 'تنظیمات ایمیل',
+  },
+  {
+    label: 'تغییر گذرواژه',
+  },
+];
+
 const Dashboard: FC<DashboardProps> = () => {
-  return <Panel>در حال تکمیل ...</Panel>;
+  const [activeTab, setActiveTab] = useState(0);
+
+  // const CurrentComponent = tabs[activeTab].component;
+  const image = '';
+
+  return (
+    <Panel>
+      <Grid container sx={{ py: 2 }}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            {image && (
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={image}
+              />
+            )}
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                مرتضی ابوالقاسمی
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                علاقه‌مند به تحقیقات علمی و پژوهشی
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">مطالعه بیشتر</Button>
+              <Button size="small">ویرایش</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={8}></Grid>
+      </Grid>
+    </Panel>
+  );
 };
 
 export default Dashboard;
