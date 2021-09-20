b25a43a40286c65079f26ba051212ebddf9acd9b
ab.reza <a.re.morteza@gmail.com>
2021-09-20 07:21:21 +0430

    fix theme coloring

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -44,32 +44,62 @@ declare module '@mui/material/styles/createTypography' {
 
 const defaultTheme = createTheme();
 
+const orange = {
+  50: '#fff4e1',
+  100: '#ffe2b4',
+  200: '#ffcf83',
+  300: '#ffbb52',
+  400: '#ffbb52',
+  500: '#ff9d12',
+  600: '#ff9110',
+  700: '#fa810e',
+  main: '#fa810e',
+  800: '#f4710d',
+  900: '#eb570b',
+};
+
+export const orangeDark = {
+  50: '#f5e9e5',
+  100: '#f0ccb8',
+  200: '#e7ac8b',
+  300: '#de8e5d',
+  main: '#de8e5d',
+  400: '#d87939',
+  500: '#d26611',
+  600: '#c9600e',
+  700: '#bc590b',
+  800: '#ae520a',
+  900: '#96460a',
+};
+
 const blue = {
-  50: '#F0F7FF',
-  100: '#C2E0FF',
-  200: '#A5D8FF',
-  300: '#66B2FF',
-  400: '#3399FF',
-  main: '#007FFF', // contrast 3.83:1
-  500: '#007FFF',
-  600: '#0072E5',
-  700: '#0059B2',
-  800: '#004C99',
-  900: '#003A75',
+  50: '#e2f7fb',
+  100: '#b8ebf5',
+  200: '#8cdfef',
+  300: '#64d1e6',
+  400: '#50c7df',
+  main: '#50c7df',
+  500: '#4abdd8',
+  600: '#44adc5',
+  700: '#3d98aa',
+  800: '#378492',
+  900: '#2a6167',
 };
+
 export const blueDark = {
-  50: '#E2EDF8',
-  100: '#CEE0F3',
-  200: '#91B9E3',
-  300: '#5090D3',
-  main: '#5090D3',
-  400: '#265D97',
-  500: '#1E4976',
-  600: '#173A5E',
-  700: '#132F4C', // contrast 13.64:1
-  800: '#001E3C',
-  900: '#0A1929',
+  50: '#e1f4fc',
+  100: '#b4e5f8',
+  200: '#84d4f1',
+  300: '#57c2e8',
+  400: '#3bb7df',
+  500: '#32abd6',
+  600: '#2b9cc2',
+  700: '#2088a8',
+  800: '#17758f',
+  main: '#17758f',
+  900: '#065363',
 };
+
 const grey = {
   50: '#F3F6F9',
   100: '#EAEEF3',
@@ -88,7 +118,7 @@ const systemFont = ['IRANSans'];
 export const getMetaThemeColor = (mode: 'light' | 'dark') => {
   const themeColor = {
     light: grey[50],
-    dark: blueDark[800],
+    dark: orangeDark[800],
   };
   return themeColor[mode];
 };
@@ -98,79 +128,85 @@ export const getDesignTokens = (mode: 'light' | 'dark') =>
     direction: 'rtl',
     palette: {
       primary: {
+        ...orange,
+        ...(mode === 'dark' && {
+          main: orange[400],
+        }),
+        divider: mode === 'dark' ? orangeDark[700] : grey[200],
+        primaryDark: orangeDark,
+        mode,
+        ...(mode === 'dark' && {
+          background: {
+            default: orangeDark[800],
+            paper: orangeDark[900],
+          },
+        }),
+        common: {
+          black: '#1D1D1D',
+        },
+        ...(mode === 'light' && {
+          text: {
+            primary: grey[900],
+            secondary: grey[800],
+          },
+        }),
+        ...(mode === 'dark' && {
+          text: {
+            primary: '#fff',
+            secondary: grey[500],
+          },
+        }),
+        grey,
+        error: {
+          50: '#FFF0F1',
+          100: '#FFDBDE',
+          200: '#FFBDC2',
+          300: '#FF99A2',
+          400: '#FF7A86',
+          500: '#FF505F',
+          main: '#EB0014', // contrast 4.63:1
+          600: '#EB0014',
+          700: '#C70011',
+          800: '#94000D',
+          900: '#570007',
+        },
+        success: {
+          50: '#E9FBF0',
+          100: '#C6F6D9',
+          200: '#9AEFBC',
+          300: '#6AE79C',
+          400: '#3EE07F',
+          500: '#21CC66',
+          600: '#1DB45A',
+          ...(mode === 'dark' && {
+            main: '#1DB45A', // contrast 6.17:1 (orangeDark.800)
+          }),
+          ...(mode === 'light' && {
+            main: '#1AA251', // contrast 3.31:1
+          }),
+          700: '#1AA251',
+          800: '#178D46',
+          900: '#0F5C2E',
+        },
+        warning: {
+          50: '#FFF9EB',
+          100: '#FFF4DB',
+          200: '#FFEDC2',
+          300: '#FFE4A3',
+          400: '#FFD980',
+          500: '#FCC419',
+          600: '#FAB005',
+          main: '#F1A204', // does not pass constrast ratio
+          700: '#F1A204',
+          800: '#DB9A00',
+          900: '#8F6400',
+        },
+      },
+      secondary: {
         ...blue,
         ...(mode === 'dark' && {
-          main: blue[400],
+          main: blue[700],
         }),
-      },
-      divider: mode === 'dark' ? blueDark[700] : grey[200],
-      primaryDark: blueDark,
-      mode,
-      ...(mode === 'dark' && {
-        background: {
-          default: blueDark[800],
-          paper: blueDark[900],
-        },
-      }),
-      common: {
-        black: '#1D1D1D',
-      },
-      ...(mode === 'light' && {
-        text: {
-          primary: grey[900],
-          secondary: grey[800],
-        },
-      }),
-      ...(mode === 'dark' && {
-        text: {
-          primary: '#fff',
-          secondary: grey[500],
-        },
-      }),
-      grey,
-      error: {
-        50: '#FFF0F1',
-        100: '#FFDBDE',
-        200: '#FFBDC2',
-        300: '#FF99A2',
-        400: '#FF7A86',
-        500: '#FF505F',
-        main: '#EB0014', // contrast 4.63:1
-        600: '#EB0014',
-        700: '#C70011',
-        800: '#94000D',
-        900: '#570007',
-      },
-      success: {
-        50: '#E9FBF0',
-        100: '#C6F6D9',
-        200: '#9AEFBC',
-        300: '#6AE79C',
-        400: '#3EE07F',
-        500: '#21CC66',
-        600: '#1DB45A',
-        ...(mode === 'dark' && {
-          main: '#1DB45A', // contrast 6.17:1 (blueDark.800)
-        }),
-        ...(mode === 'light' && {
-          main: '#1AA251', // contrast 3.31:1
-        }),
-        700: '#1AA251',
-        800: '#178D46',
-        900: '#0F5C2E',
-      },
-      warning: {
-        50: '#FFF9EB',
-        100: '#FFF4DB',
-        200: '#FFEDC2',
-        300: '#FFE4A3',
-        400: '#FFD980',
-        500: '#FCC419',
-        600: '#FAB005',
-        main: '#F1A204', // does not pass constrast ratio
-        700: '#F1A204',
-        800: '#DB9A00',
-        900: '#8F6400',
       },
     },
     shape: {
@@ -189,7 +225,7 @@ export const getDesignTokens = (mode: 'light' | 'dark') =>
         fontWeight: 800,
         lineHeight: 78 / 70,
         ...(mode === 'light' && {
-          color: blueDark[900],
+          color: orangeDark[900],
         }),
       },
       h2: {
@@ -197,7 +233,7 @@ export const getDesignTokens = (mode: 'light' | 'dark') =>
         fontSize: 'clamp(1.5rem, 0.9643rem + 1.4286vw, 2.25rem)',
         fontWeight: 800,
         lineHeight: 44 / 36,
-        color: mode === 'dark' ? grey[200] : blueDark[700],
+        color: mode === 'dark' ? grey[200] : orangeDark[700],
       },
       h3: {
         fontSize: defaultTheme.typography.pxToRem(36),
