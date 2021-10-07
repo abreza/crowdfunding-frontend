a73054030348e2a390a571e391f300fd57a2c028
ab.reza <a.re.morteza@gmail.com>
2021-10-08 00:38:40 +0330

    init pwa

--- next.config.js
+++ next.config.js
@@ -5,19 +5,28 @@
 const withImages = require('next-images');
 const nextTranslate = require('next-translate');
 
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 module.exports = withImages(
-  nextTranslate({
-    images: {
-      disableStaticImages: true,
-      domains: [
-        'bankautomationnews.com',
-        'localhost:3000',
-        '127.0.0.1',
-        'crowdfunding.mamalan.ir',
-      ],
-    },
-    env: {
-      PUBLIC_URL: '',
-    },
-  })
+  withPWA(
+    nextTranslate({
+      pwa: {
+        dest: 'public',
+        runtimeCaching,
+      },
+      images: {
+        disableStaticImages: true,
+        domains: [
+          'bankautomationnews.com',
+          'localhost:3000',
+          '127.0.0.1',
+          'crowdfunding.mamalan.ir',
+        ],
+      },
+      env: {
+        PUBLIC_URL: '',
+      },
+    })
+  )
 );
