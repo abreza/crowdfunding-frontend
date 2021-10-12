5c4cda4d22837ce73421ad2bb8a0740ea349804a
Morteza Abolghasemi <a.re.morteza@gmail.com>
2021-10-12 11:19:04 +0330

    add timeline

---
+++ src/components/organisms/projectSections/ProjectTimeline.tsx
@@ -0,0 +1,31 @@
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import { FC, useContext } from 'react';
+import { ProjectContext } from 'src/contexts/ProjectContext';
+
+export const ProjectTimeline: FC = () => {
+  const { timelines = [] } = useContext(ProjectContext);
+  return (
+    <>
+      <Timeline position="alternate">
+        {timelines.map((timeline, index) => (
+          <TimelineItem key={index}>
+            <TimelineOppositeContent color="text.secondary">
+              {timeline.date}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>{timeline.name}</TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
+    </>
+  );
+};
--- src/components/organisms/projectSections/projectTabs/ProjectDescription.tsx
+++ src/components/organisms/projectSections/projectTabs/ProjectDescription.tsx
@@ -1,18 +1,27 @@
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { ProjectContext } from 'src/contexts/ProjectContext';
 import { FC, useContext } from 'react';
+import { ProjectTimeline } from '../ProjectTimeline';
+import { Box } from '@mui/system';
 
 type ProjectDescriptionProps = {
   description: string;
 };
 
 const ProjectDescription: FC<ProjectDescriptionProps> = () => {
-  const { summary } = useContext(ProjectContext);
+  const { summary, projectFirstIdea } = useContext(ProjectContext);
 
   return (
-    <div>
+    <Stack spacing={2}>
       <Typography variant="body1">{summary}</Typography>
-    </div>
+      <ProjectTimeline />
+      {projectFirstIdea && (
+        <Box>
+          <Typography variant="h2">ایده اولیه پروژه</Typography>
+          <Typography>{projectFirstIdea}</Typography>
+        </Box>
+      )}
+    </Stack>
   );
 };
 
