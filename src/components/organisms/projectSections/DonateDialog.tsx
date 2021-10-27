01d0fdd24dd1ac1662d53438097bfa30f6f712a2
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-10-27 11:48:09 +0330

    Fix prettier error

--- src/components/organisms/projectSections/DonateDialog.tsx
+++ src/components/organisms/projectSections/DonateDialog.tsx
@@ -35,7 +35,8 @@ export const DonateDialog: FC<{ open: boolean; handleClose: () => void }> = ({
       donateDto: {
         amount,
         projectId: id as string,
-        callbackUrl: 'https://crowdfunding.mamalan.ir/payment/result/' + id + '/',
+        callbackUrl:
+          'https://crowdfunding.mamalan.ir/payment/result/' + id + '/',
       },
     })
       .unwrap()
